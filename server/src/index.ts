342aff6e5e2cba6415d6169f3c5dc3bb268ef72a
HeRo3S <hungid6a1@gmail.com>
2023-01-13 20:58:57 +0700

    created migration schema for database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import dotenv from 'dotenv';
+import express from 'express';
 
-const PORT = 5000;
+// *setup dotenv
+dotenv.config();
+
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.status(200).send("Hello World");
+const PORT = 5000;
+
+app.get('/test', (req, res) => {
+  res.status(200).send('Hello World');
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
