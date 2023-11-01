d1c007af1a895773d254781e6125e05e03f894c2
HeRo3Sm <hungid6a1@gmail.com>
2023-11-02 00:49:50 +0700

    rechanged cors logic

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,4 +1,4 @@
 export default [
   'http://localhost:3000',
-  'https://forumt-frontend.onrender.com/',
+  'https://forumt-frontend.onrender.com',
 ];
--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -3,7 +3,7 @@ import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (origin && allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
