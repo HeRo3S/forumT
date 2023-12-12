6eb4a4911db6d9b1282474e1f19f71947d02fc1f
HeRo3Sm <hungid6a1@gmail.com>
2023-12-13 03:36:21 +0700

    added domain for allowedOrigin

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,3 +1,7 @@
 import 'dotenv/config';
 
-export default [process.env.CLIENT_DEV_URL, process.env.CLIENT_PROD_URL];
+export default [
+  process.env.CLIENT_DEV_URL,
+  process.env.CLIENT_PROD_URL,
+  process.env.CLIENT_DOMAIN,
+];
