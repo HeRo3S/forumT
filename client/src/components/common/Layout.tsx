c9a471c8085a7453a3f38d3c248a4d65b77be8d9
HeRo3Sm <hungid6a1@gmail.com>
2024-01-01 15:17:42 +0700

    added some reponsive value for layout

--- client/src/components/common/Layout.tsx
+++ client/src/components/common/Layout.tsx
@@ -2,18 +2,28 @@ import Grid, { GridProps } from '@mui/material/Grid';
 import Container, { ContainerProps } from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
 
-export const PageContainer = styled(Grid)<GridProps>({});
+export const PageContainer = styled(Grid)<GridProps>({
+  justifyContent: 'center',
+});
 
 export const LeftBarContainer = styled(Grid)<GridProps>(({ theme }) => ({
   position: 'sticky',
   top: '50px',
   height: '95vh',
   backgroundColor: theme.palette.grey[400],
+  [theme.breakpoints.down('lg')]: {
+    display: 'none',
+  },
 }));
 
 export const ContentContainer = styled(Container)<ContainerProps>(
   ({ theme }) => ({
-    width: '800px',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '800px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      minWidth: '250px',
+    },
     marginBottom: '20px',
     padding: '0 0',
     cursor: 'pointer',
@@ -31,4 +41,7 @@ export const RightBarContainer = styled(Grid)<GridProps>(({ theme }) => ({
   position: 'sticky',
   top: '50px',
   height: '90vh',
+  [theme.breakpoints.down('lg')]: {
+    display: 'none',
+  },
 }));
